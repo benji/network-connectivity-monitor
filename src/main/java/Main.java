977f2b0d768dd9e80d7a048c7b0a72e9b15165ab
Benjamin Jaton <benjamin.jaton@gmail.com>
2019-12-01 18:52:54 -0800

    Using local TZ

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,32 +1,54 @@
+import java.io.IOException;
 import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.time.Instant;
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Main {
 	static String host = System.getProperty("monitoring.host", "www.google.com");
 	static Integer pingTimeout = Integer.getInteger("monitoring.ping.timeout", 10000);
+	static Integer interval = Integer.getInteger("monitoring.interval", 1000);
 
-	public static void main(String[] args) throws Exception {
+	static DateTimeFormatter iso8601DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSX")
+			.withZone(OffsetDateTime.now().getOffset().normalized());
+
+	public static void main(String[] args) throws InterruptedException {
+
 		java.security.Security.setProperty("networkaddress.cache.ttl", "0");
 
 		long start, stop;
+		System.out.println("datetime,dns,ping");
 
 		while (true) {
+			Instant now = Instant.now();
+			long elapsedDns;
 			InetAddress addr = null;
 			start = System.currentTimeMillis();
-			addr = InetAddress.getByName(host);
-			stop = System.currentTimeMillis();
-			long elapsedDns = stop - start;
-
-			long elapsedPing;
-			start = System.currentTimeMillis();
-			if (addr.isReachable(pingTimeout)) {
+			try {
+				addr = InetAddress.getByName(host);
 				stop = System.currentTimeMillis();
-				elapsedPing = stop - start;
-			} else {
-				elapsedPing = -1;
+				elapsedDns = stop - start;
+			} catch (UnknownHostException e) {
+				elapsedDns = -1; // failed DNS resolution
 			}
 
-			System.out.println(elapsedDns + "," + elapsedPing);
-			Thread.sleep(1000);
+			long elapsedPing = -1;
+
+			if (elapsedDns != -1) {
+				start = System.currentTimeMillis();
+				try {
+					if (addr.isReachable(pingTimeout)) {
+						stop = System.currentTimeMillis();
+						elapsedPing = stop - start;
+					}
+				} catch (IOException e) {
+					e.printStackTrace(); // to syserr
+				}
+			}
+
+			System.out.println(iso8601DateTimeFormatter.format(now) + "," + elapsedDns + "," + elapsedPing);
+			Thread.sleep(interval);
 		}
 	}
 }
